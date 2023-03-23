8bfe8eccff3b8024c82bf5e2b6909b9904ad0a41
JOSEPH <lena.boya26@gmail.com>
2023-03-23 21:14:40 +0100

    mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-
 function NavBar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
@@ -18,21 +18,29 @@ function NavBar() {
 
   return (
     <nav
-  className={`fixed w-full z-10 transition-all duration-300 py-4 ${
-    isScrolled
-      ? 'backdrop-filter backdrop-blur-sm bg-opacity-50'
-      : 'bg-transparent'
-  }`}
->
-
+      className={`fixed w-full z-10 transition-all duration-300 py-4 ${
+        isScrolled
+          ? 'backdrop-filter backdrop-blur-sm bg-opacity-50'
+          : 'bg-transparent'
+      }`}
+    >
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
-    
-            
           <span></span>
 
-          <div className="hidden md:flex space-x-4">
-            <a href="#home" className="relative inline-block px-3 py-2 text-sm font-medium ">
+          <button
+            className="md:hidden"
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          >
+            <span>☰</span>
+          </button>
+
+          <div
+            className={`${
+              mobileMenuOpen ? 'flex' : 'hidden'
+            } md:flex space-x-4`}
+          >
+            <a href="#home" className="relative inline-block px-3 py-2 text-sm font-medium">
               HOME
               <div className="relative">
                 <div className="line absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 transition-transform duration-200"></div>
@@ -40,14 +48,14 @@ function NavBar() {
             </a>
 
             <a href="#arts" className="relative inline-block px-3 py-2 text-sm font-medium">
-         ARTS
+              ARTS
               <div className="relative">
                 <div className="line absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 transition-transform duration-200"></div>
               </div>
             </a>
 
             <a href="#contact" className="relative inline-block px-3 py-2 text-sm font-medium">
-             CONTACT
+              CONTACT
               <div className="relative">
                 <div className="line absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 transition-transform duration-200"></div>
               </div>
